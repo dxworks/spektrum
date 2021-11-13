879948a82ed51ffcf654a5091614f5825c25342b
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-13 11:36:24 +0200

    Initial commit

---
+++ src/main/kotlin/me/drbaxr/Main.kt
@@ -0,0 +1,8 @@
+package me.drbaxr
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+}
