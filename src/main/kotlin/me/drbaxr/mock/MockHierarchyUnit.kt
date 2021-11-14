6330634a919bb91eda66d1967613ce4026553506
Alexandru Florea <alexflorea16000@gmail.com>
2021-11-14 18:27:04 +0200

    Mock data reading fix

--- src/main/kotlin/me/drbaxr/mock/MockHierarchyUnit.kt
+++ src/main/kotlin/me/drbaxr/mock/MockHierarchyUnit.kt
@@ -2,8 +2,8 @@ package me.drbaxr.mock
 
 data class MockHierarchyUnit(
     var identifier: String,
-    var children: MutableSet<MockHierarchyUnit>,
+    var children: MutableSet<MockHierarchyUnit>?,
     var type: String,
-    var isTestable: Boolean = false,
-    var callers: MutableSet<String>
+    var isTestable: Boolean? = false,
+    var callers: MutableSet<String>?
 )
