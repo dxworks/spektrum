23f51056153ffc40fb72fc4e23c85c8692b21add
Alexandru Florea <alexflorea16000@gmail.com>
2022-03-06 16:54:34 +0200

    Test for C# model adapter

---
+++ src/test/kotlin/me/drbaxr/spektrum/test/CSModelAdapterTest.kt
@@ -0,0 +1,24 @@
+package me.drbaxr.spektrum.test
+
+import me.drbaxr.spektrum.adapters.CSModelAdapter
+import me.drbaxr.spektrum.test.util.TestUtil
+import org.junit.Test
+import java.io.FileReader
+import kotlin.test.assertTrue
+
+class CSModelAdapterTest {
+    @Test
+    fun testCSModel() {
+        val adapter = CSModelAdapter("src/test/resources/inputs/honeydewCSModel_in.json")
+        val actual = adapter.adapt()
+        val expected = TestUtil.getMock(FileReader("src/test/resources/outputs/honeydewCSModel_exp.json"))
+
+        var eq = true
+        expected.forEach { exp ->
+            val act = actual.find { it.identifier == exp.identifier }
+            if (act?.isEqual(exp) != true)
+                eq = false
+        }
+        assertTrue { eq }
+    }
+}
