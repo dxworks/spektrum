f27ad3f9b8c334e5d3d385271a86b0e03a160e20
Alexandru Florea <alexflorea16000@gmail.com>
2022-04-16 20:59:33 +0300

    Sample use case for GroovyShell

--- src/test/kotlin/me/drbaxr/spektrum/test/rules/script/ScriptRuleTest.kt
+++ src/test/kotlin/me/drbaxr/spektrum/test/rules/script/ScriptRuleTest.kt
@@ -1,17 +1,38 @@
 package me.drbaxr.spektrum.test.rules.script
 
 import groovy.lang.GroovyShell
+import me.drbaxr.spektrum.flexible.RelevantInformation
+import me.drbaxr.spektrum.flexible.adapters.CSModelAdapter
+import org.junit.Before
 import org.junit.Test
 import java.io.File
 
 class ScriptRuleTest {
 
+    @Before
+    fun init() {
+        CSModelAdapter("src/test/resources/inputs/honeydewCSModel_in.json").adapt()
+    }
+
     @Test
     fun test() {
-        GroovyShell()
-            .parse(File("scripts/test.groovy"))
-            .invokeMethod("hello_world", null)
+        val scriptPath = "scripts/test.groovy"
+        val functionName = "evaluate"
 
-        TODO("Rule that you can read from a groovy file")
+        val unitIdentifier = "HoneydewCoreTest/IO/Writers/Exporters/ExportUtilsTests.cs->" +
+                "HoneydewCoreTest.IO.Writers.Exporters->" +
+                "ExportUtilsTests->" +
+                "CsvCountPerLine_ShouldReturn0_WhenNoNumberIsDetected#"
+
+        val value = GroovyShell()
+            .parse(File(scriptPath))
+            .invokeMethod(
+                functionName,
+                RelevantInformation.getCSImportModelInformation(unitIdentifier)
+            )
+
+        println(value)
+
+        TODO("make ScriptCSRule and test that shit")
     }
 }
