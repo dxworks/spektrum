6110cdb541b3831c0196d97e2ce1ecc702303e28
Alexandru Florea <alexflorea16000@gmail.com>
2022-04-17 11:08:50 +0300

    Completely removed CSRulesUtil and replaced functionality with RelevantInformation

--- src/main/kotlin/me/drbaxr/spektrum/flexible/identifiers/rules/cs/HasSomeUsingStatements.kt
+++ src/main/kotlin/me/drbaxr/spektrum/flexible/identifiers/rules/cs/HasSomeUsingStatements.kt
@@ -7,9 +7,9 @@ import me.drbaxr.spektrum.flexible.identifiers.rules.Rule
 class HasSomeUsingStatements(val statements: Set<String>) : Rule {
 
     override fun isRespectedBy(unit: HierarchyUnit): Boolean {
-        val importedModelClass = CSRulesUtil.findMethodClassInModel(RelevantInformation.importCSModel, unit)
+        val unitInfo = RelevantInformation.getCSImportModelInformation(unit.identifier)
 
-        return statements.any { statement -> importedModelClass.usingStatements.any { it == statement } }
+        return statements.any { statement -> unitInfo.cls.usingStatements.any { it == statement } }
     }
 
 }
