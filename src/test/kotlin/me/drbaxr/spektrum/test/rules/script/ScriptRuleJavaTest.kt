1afe3a49a75458eb3d47adb1891e852f976476e7
Alexandru Florea <alexflorea16000@gmail.com>
2022-04-29 15:00:43 +0300

    Script rule java test

---
+++ src/test/kotlin/me/drbaxr/spektrum/test/rules/script/ScriptRuleJavaTest.kt
@@ -0,0 +1,28 @@
+package me.drbaxr.spektrum.test.rules.script
+
+import me.drbaxr.spektrum.fixed.model.HierarchyUnit
+import me.drbaxr.spektrum.flexible.adapters.java.JavaModelAdapter
+import me.drbaxr.spektrum.flexible.identifiers.rules.java.ScriptRuleJava
+import org.junit.Before
+import org.junit.Test
+
+class ScriptRuleJavaTest {
+    @Before
+    fun init() {
+        JavaModelAdapter("src/test/resources/inputs/jafaxInsiderJavaModel_in.json").adapt()
+    }
+
+    @Test
+    fun testScriptRuleJavaTrue() {
+        val identifier = "D:\\PersonalProjects\\jafax\\src\\test\\resources\\insider${HierarchyUnit.childSeparator}" +
+                "bla.bla${HierarchyUnit.childSeparator}" +
+                "Test1${HierarchyUnit.childSeparator}" +
+                "amethod()"
+
+        val unit = HierarchyUnit(identifier, mutableSetOf(), "METHOD")
+
+        val rule = ScriptRuleJava("rules/java/rule.groovy")
+
+        rule.isRespectedBy(unit)
+    }
+}
